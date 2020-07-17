d9d3c06bb2c262ee79f4ac5eddd1913b8073a0e4
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-17 00:06:23 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@artist_count={}
+    @@genre_count={}
+
+attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist,genre ) 
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@count+=1
+    @@artists<<artist
+    @@genres<<genre
+    end
+
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    def self.genre_count
+    end
+    def self.artist_count
+    end
+end
